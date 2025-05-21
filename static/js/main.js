60046e5a1e93029f680007cfa0dd1f8f8942502e
SAI KRISHNA KOTHA <kotha.sai.krishna.in@gmail.com>
2025-05-21 20:12:22 +0530

    feature: added random colors

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,22 @@
-let colors = ['black', 'blue', 'yellow', 'red'];
-let currentIndex = 0;
+// let colors = ['black', 'blue', 'yellow', 'red'];
+// let currentIndex = 0;
 
 function ChangeColor() {
-    document.body.style.backgroundColor = colors[currentIndex];
-    document.getElementById('current-color-id').innerHTML = colors[currentIndex];
+    const rgbValues = getRandomRGB();
+    document.body.style.backgroundColor = rgbValues;
+    document.getElementById('current-color-id').innerHTML = rgbValues;
     // if (colors[currentIndex] == "black" || colors[currentIndex] == "blue"){
     //     document.querySelector('.current-color').style.color = 'white';
     // } else{
     //     document.querySelector('.current-color').style.color = 'black';
     // }
-    currentIndex = (currentIndex + 1) % colors.length;
+    document.querySelector('.current-color').style.color = rgbValues;
+    // currentIndex = (currentIndex + 1) % colors.length;
 }
+function getRandomRGB(){
+    const r = Math.random()*256;
+    const g = Math.random()*256;
+    const b = Math.random()*256;
+    return `rgb(${Math.round(r)},${Math.round(g)},${Math.round(b)})`
+}
+console.log(getRandomRGB());
