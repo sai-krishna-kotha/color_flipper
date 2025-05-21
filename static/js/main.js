48e7ab1b7f48d288a08b92bc2f3edb1ce0726c63
SAI KRISHNA KOTHA <kotha.sai.krishna.in@gmail.com>
2025-05-21 15:27:58 +0530

    ui updated

--- static/js/main.js
+++ static/js/main.js
@@ -4,10 +4,10 @@ let currentIndex = 0;
 function ChangeColor() {
     document.body.style.backgroundColor = colors[currentIndex];
     document.getElementById('current-color-id').innerHTML = colors[currentIndex];
-    if (colors[currentIndex] == "black" || colors[currentIndex] == "blue"){
-        document.querySelector('.current-color').style.color = 'white';
-    } else{
-        document.querySelector('.current-color').style.color = 'black';
-    }
+    // if (colors[currentIndex] == "black" || colors[currentIndex] == "blue"){
+    //     document.querySelector('.current-color').style.color = 'white';
+    // } else{
+    //     document.querySelector('.current-color').style.color = 'black';
+    // }
     currentIndex = (currentIndex + 1) % colors.length;
 }
